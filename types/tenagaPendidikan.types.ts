5168c5d8294cc72ac4ed3ffb7f36ccd90ecb0f73
andypratama3 <andypratama1211@gmail.com>
2026-01-10 02:41:43 +0800

    Fix Update Tenaga Pendidikan

--- types/tenagaPendidikan.types.ts
+++ types/tenagaPendidikan.types.ts
@@ -1,6 +1,30 @@
 export interface TenagaPendidikan {
-    name: string;
-    jabatan: string;
-    foto: string;
-    slug: string;
+  name: string
+  jabatan: string
+  foto: string
+  slug: string
+}
+
+export interface Staff {
+  name: string
+  position: string
+  image: string
+  category: string
+  slug: string
+  description: string
+  id: string
+}
+
+export interface StrukturNode {
+  id: string
+  name: string
+  slug: string
+  staff: Staff[]
+  children: StrukturNode[]
+}
+
+export interface TenagaPendidikanResponse {
+  success: boolean
+  message: string
+  data: StrukturNode[]
 }
